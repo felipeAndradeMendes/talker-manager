bd7cc872b14f78fa72ea750977b067d344e6fdf5
Felipe Mendes <felipemadruga@gmail.com>
2023-05-21 17:57:43 -0300

    tentativa 06 - muda connection host:localhost e port:3001

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: 'localhost',
-  port: 3306,
+  port: 3001,
   user: 'root',
   password: 'password',
   database: 'TalkerDB',
